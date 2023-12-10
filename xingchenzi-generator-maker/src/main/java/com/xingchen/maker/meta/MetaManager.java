d1f4618c1560593723d12daa1025a1d1af7acc0f
linxun1209 <3056726345@qq.com>
2023-12-10 14:44:09 +0800

    修复代码生成工具的打jar的bug 原因是meta.json的文件路径写错     "inputRootPath": "E:/后端大佬成才之路/星辰代码生成项目/xingchenzi-generator/yuzi-generator-demo-projects/acm-template-pro", 应该把//改成/

--- xingchenzi-generator-maker/src/main/java/com/xingchen/maker/meta/MetaManager.java
+++ xingchenzi-generator-maker/src/main/java/com/xingchen/maker/meta/MetaManager.java
@@ -23,7 +23,7 @@ public class MetaManager {
     }
 
     private static Meta initMeta() {
-        String metaJson = ResourceUtil.readUtf8Str("meta.json");
+        String metaJson = ResourceUtil.readUtf8Str("templates/meta.json");
         Meta newMeta = JSONUtil.toBean(metaJson, Meta.class);
         Meta.FileConfig fileConfig = newMeta.getFileConfig();
         // todo 校验和处理默认值
